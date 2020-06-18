596a43ab48d6707f9efab462d9971a61c656538b
Yazan Aamer <yazan.aamer@outlook.com>
2020-06-18 17:01:22 +0300

    Initial Commit B

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -11,10 +11,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    void YznMohsenFunction()
-    {}
-
-    void BaselFunction()
-    {}
- void josephFun(){}
 }
