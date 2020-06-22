0eb41f2a7258d6bfd06e473bc1608c363c8f058e
Yazan Aamer <yazan.aamer@outlook.com>
2020-06-22 13:47:00 +0300

    working on mp4 parser

--- app/src/main/java/com/example/mediaplayer/ContainerManager/Parser/MB4Parser.java
+++ app/src/main/java/com/example/mediaplayer/ContainerManager/Parser/MB4Parser.java
@@ -3,6 +3,7 @@ package com.example.mediaplayer.ContainerManager.Parser;
 import com.example.mediaplayer.Data.Container.Container;
 
 public class MB4Parser extends Parser {
+
     public MB4Parser(Container container) {
         super(container);
     }
--- app/src/main/java/com/example/mediaplayer/ContainerManager/Parser/Parser.java
+++ app/src/main/java/com/example/mediaplayer/ContainerManager/Parser/Parser.java
@@ -3,12 +3,14 @@ package com.example.mediaplayer.ContainerManager.Parser;
 import com.example.mediaplayer.Data.Container.Container;
 
 abstract public class Parser {
-Container container;
+
+    Container container;
 
 
     public Parser(Container container) {
         this.container = container;
     }
+
     abstract public void Parse();
 
     public Container getContainer(){
