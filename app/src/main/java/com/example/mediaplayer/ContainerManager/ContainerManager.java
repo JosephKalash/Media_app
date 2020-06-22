b3ea8669c0f26a359e542d645bd2b7f1ac4eddb0
Yazan Aamer <yazan.aamer@outlook.com>
2020-06-22 13:58:02 +0300

    added a 'getRawFile' method, this method will return raw file which will be parsed in the parser subclasses

--- app/src/main/java/com/example/mediaplayer/ContainerManager/ContainerManager.java
+++ app/src/main/java/com/example/mediaplayer/ContainerManager/ContainerManager.java
@@ -4,6 +4,8 @@ import com.example.mediaplayer.ContainerManager.Decoder.Decoder;
 import com.example.mediaplayer.ContainerManager.Parser.Parser;
 import com.example.mediaplayer.Data.Container.Container;
 
+import java.io.File;
+
 public class ContainerManager {
     Container container;
     Parser parser;
@@ -15,6 +17,10 @@ public class ContainerManager {
     public Container getContainer(){
         return container;
     }
+    public File getCurrentRawFile() {
+        return null;
+    }
+
     public void Decode(){ }
     public void Parse(){}
     public void StartManaging(){}
