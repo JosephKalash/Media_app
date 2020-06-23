7e77b582ee77884f39b6ad88ab79ede562266b91
Yazan Aamer <yazan.aamer@outlook.com>
2020-06-23 16:56:53 +0300

    Defined the model for sample tebles, these classes are all put in mp4 package for the sake of ease access

--- app/src/main/java/com/example/mediaplayer/ContainerManager/Parser/mp4parser/Stco.java
+++ app/src/main/java/com/example/mediaplayer/ContainerManager/Parser/mp4parser/Stco.java
@@ -1,4 +1,25 @@
 package com.example.mediaplayer.ContainerManager.Parser.mp4parser;
 
+import java.util.List;
+
 public class Stco {
+
+    private int mEntryCount;
+    private List<Integer> mEntries;
+
+    Stco(int entryCount, List<Integer> list) {
+        this.mEntryCount = entryCount;
+        this.mEntries = list;
+    }
+
+    int getChunkOffset(int i) {
+        return mEntries.get(i);
+    }
+
+    int getEntriesCount() {
+        return mEntryCount;
+    }
+
+
+
 }
--- app/src/main/java/com/example/mediaplayer/ContainerManager/Parser/mp4parser/Stsc.java
+++ app/src/main/java/com/example/mediaplayer/ContainerManager/Parser/mp4parser/Stsc.java
@@ -1,4 +1,28 @@
 package com.example.mediaplayer.ContainerManager.Parser.mp4parser;
 
+import java.util.List;
+
 public class Stsc {
+    private int mEntriesCount;
+    private List<Integer> mFirstChunk;
+    private List<Integer> mSamplesPerChunk;
+
+    Stsc(int entriesCount, List<Integer> firstChunk, List<Integer> samplesPerChuk) {
+        mEntriesCount = entriesCount;
+        mFirstChunk = firstChunk;
+        mSamplesPerChunk = samplesPerChuk;
+    }
+
+    public int getmEntriesCount() {
+        return mEntriesCount;
+    }
+
+    public int getmFirstChunk(int i) {
+        return mFirstChunk.get(i);
+    }
+
+    public int getmSamplesPerChunk(int i) {
+        return mSamplesPerChunk.get(i);
+    }
+
 }
