d85d328a98568ac29e8af10080722a1b1063e03f
josephKalash <39829281+josefKalash@users.noreply.github.com>
2020-06-28 01:26:46 +0300

    deleted old activity_main and main2.xml

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -73,10 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (requestCode == 1){
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                //load data here
                 //for first time data will be loaded here
-                //then it will be loaded in splash screen
-                //because if we could not have permission then we could not load data in splash screen window
                 stReader.loadMediaFiles(this);
             }
         }
